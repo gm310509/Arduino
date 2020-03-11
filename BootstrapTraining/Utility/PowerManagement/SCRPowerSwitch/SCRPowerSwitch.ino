92b13476e95498e2993929f135d3dd9fa0d80fa8
gm310509 <gm310509@gmail.com>
2020-03-11 19:48:25 +1100

    Added comments and changed the power control pin to 2 (to match the circuit diagrams)

--- BootstrapTraining/Utility/PowerManagement/SCRPowerSwitch/SCRPowerSwitch.ino
+++ BootstrapTraining/Utility/PowerManagement/SCRPowerSwitch/SCRPowerSwitch.ino
@@ -1,20 +1,20 @@
-#define POWER_CONTROL_PIN   7
-
+#define POWER_CONTROL_PIN   2
 
 void setup() {
   // put your setup code here, to run once:
 
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(POWER_CONTROL_PIN, OUTPUT);
-  digitalWrite(POWER_CONTROL_PIN, HIGH);      // Turn on the transistor to allow power to flow.
+  digitalWrite(POWER_CONTROL_PIN, HIGH);      // Turn on the transistor to maintain the power flow.
 
   Serial.begin(9600);
   while (!Serial) {
     delay(10);
   }
-  Serial.println("SCR Controlled power switch - Ready");
+  Serial.println("Arduino Controlled power switch - Ready");
 }
 
+// Rapidly blink the inbuilt LED to show that the Arduino is "alive"
 void blinkLED(int duration) {
   digitalWrite(LED_BUILTIN, HIGH);
   delay (duration);
@@ -23,16 +23,17 @@ void blinkLED(int duration) {
 }
 
 void loop() {
-  
+
   Serial.println("Operations mode - rapid blink");
   for (int i = 0; i < 10; i++) {
     blinkLED(150);
   }
 
   Serial.println("Shutting down mode");
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);            // Signal that we are about to shutdown by holding the inbuilt LED on for two seconds.
   delay (2000);
-  digitalWrite(POWER_CONTROL_PIN, LOW);      // Turn on the transistor to allow power to flow.
-  digitalWrite(LED_BUILTIN, LOW);
-  delay (250);
+  digitalWrite(POWER_CONTROL_PIN, LOW);       // Turn off the transistor to terminate power flow.
+
+  digitalWrite(LED_BUILTIN, LOW);             // We should never get here ('cos the power has been turned off), but just in case,
+  delay (250);                                // repeat the rapid led blinking to show that we are still "alive".
 }
