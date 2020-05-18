e3b181d2f8b958c8594ada0f5c71ce6be9b84902
gm310509 <gm310509@gmail.com>
2020-05-18 15:14:11 +1000

    Added final project configuration compile option. Added position servo to Center option. Increased range from 1 through 9 (20 -> 180 degrees) to 0 through 9 (0 -> 180 degrees)

--- Kits/TA0135-4WD-Car/Servo/Servo.ino
+++ Kits/TA0135-4WD-Car/Servo/Servo.ino
@@ -4,7 +4,17 @@
 *2017.9.14 *
 *Copyright*
 ****************/
+
+// Uncomment this line to enable a configuration of the test program
+// that mimics the final configurations.
+#define FINAL_PROJECT_CONFIG
+
+#ifdef FINAL_PROJECT_CONFIG
+int servopin = 9;   //Define the Servo Pin 9 in the final configuration for Servo signal
+#else
 int servopin=3;//Define the Servo Pin3 for Servo signal
+#endif
+
 int myangle;//Define Var angle
 int pulsewidth;//Define Pulsewidth
 int val;
@@ -20,12 +30,20 @@ void setup()
 {
   pinMode(servopin,OUTPUT);//Set Servo Pin Outpout Mode
   Serial.begin(9600);
+  while (!Serial) {
+    delay(1);
+  }
   Serial.println("servo=o_seral_simple ready" ) ;
 }
 void loop()//1 - 9 corresponds to 0 - 180^ degree
 {
   val=Serial.read();//Read from Serial port
-  if(val>'0'&&val<='9')
+  if (val == 'c' || val == 'C') {
+    Serial.println("Moving servo to center 90°");
+    for (int i = 0; i <= 50; i++) {
+      servopulse(servopin, 90);
+    }
+  } else if(val>='0'&&val<='9')
   {
     val=val-'0';
     val=val*(180/9);
