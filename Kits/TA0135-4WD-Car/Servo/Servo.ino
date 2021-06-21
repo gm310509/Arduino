826638158e34d5cf87c52384478112b559980951
gm310509 <gm310509@gmail.com>
2021-06-21 17:45:29 +0800

    Added help message that shows how to drive the program. Added some error checking.

--- Kits/TA0135-4WD-Car/Servo/Servo.ino
+++ Kits/TA0135-4WD-Car/Servo/Servo.ino
@@ -21,7 +21,7 @@ int servopin=3;//Define the Servo Pin3 for Servo signal
 
 int myangle;//Define Var angle
 int pulsewidth;//Define Pulsewidth
-int val;
+
 void servopulse(int servopin,int myangle)//PWM modulation
 {
   pulsewidth=(myangle*11)+500;//Translate to the range: 500-2480 Pulsewidth
@@ -29,7 +29,17 @@ void servopulse(int servopin,int myangle)//PWM modulation
   delayMicroseconds(pulsewidth);//Set pulsewidth microseconds
   digitalWrite(servopin,LOW);//Set Servo Pin Low 
   delay(20-pulsewidth/1000); // 20ms - Pulsewidth/1000
-} 
+}
+
+
+void outputHelp() {
+  Serial.println("Enter 'C' to center the servo");
+  Serial.println("Enter a digit (N) '0' through '9' to position the servo at N * 180/9° (i.e. N * 20°)");
+  Serial.println();
+}
+
+
+
 void setup()
 {
   pinMode(servopin,OUTPUT);//Set Servo Pin Outpout Mode
@@ -38,25 +48,31 @@ void setup()
     delay(1);
   }
   Serial.println("servo=o_seral_simple ready" ) ;
+  outputHelp();
 }
+
+
 void loop()//1 - 9 corresponds to 0 - 180^ degree
 {
+ char val;
   val=Serial.read();//Read from Serial port
   if (val == 'c' || val == 'C') {
     Serial.println("Moving servo to center 90°");
     for (int i = 0; i <= 50; i++) {
       servopulse(servopin, 90);
     }
-  } else if(val>='0'&&val<='9')
-  {
+  } else if(val>='0'&&val<='9')  {
     val=val-'0';
     val=val*(180/9);
     Serial.print("moving servo to ");
     Serial.print(val,DEC); 
-    Serial.println();
+    Serial.println("°");
     for(int i=0;i<=50;i++) //wait for completion
     {
      servopulse(servopin,val);
     }
+  } else if (val != '\n' && val != '\r' && val != -1) {
+    Serial.print("\nInvalid input: '"); Serial.print(val); Serial.println("'");
+    outputHelp();
   }
 }
